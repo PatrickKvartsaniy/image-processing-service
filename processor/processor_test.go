564834c95b259a58ef22c4b81530d4393541a830
PatrickKvartsaniy <kvartsaniy@gmail.com>
2020-08-09 18:51:25 +0300

    test: image processor

---
+++ processor/processor_test.go
@@ -0,0 +1,31 @@
+package processor_test
+
+import (
+	"bytes"
+	"github.com/PatrickKvartsaniy/image-processing-service/model"
+	"github.com/PatrickKvartsaniy/image-processing-service/processor"
+	"github.com/stretchr/testify/assert"
+	"image"
+	"os"
+	"testing"
+)
+
+func TestProcessor_Resize(t *testing.T) {
+	proc := processor.NewImageProcessor()
+	src, err := os.Open("testdata/gopher.png")
+	assert.NoError(t, err)
+
+	var b bytes.Buffer
+	params := model.SizeInput{
+		Width: 100,
+		Height: 100,
+	}
+
+	err = proc.Resize(src, &b, params)
+	assert.NoError(t, err)
+
+	img, _, err := image.DecodeConfig(&b)
+	assert.NoError(t, err)
+	assert.Equal(t, 100, img.Height)
+	assert.Equal(t, 100, img.Width)
+}
