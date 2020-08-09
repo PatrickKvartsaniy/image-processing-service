1fea52a5da0d1bd60a23a5630e9a2bdb3400027b
PatrickKvartsaniy <kvartsaniy@gmail.com>
2020-08-09 18:04:58 +0300

    test: models methods

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/99designs/gqlgen v0.11.3
 	github.com/disintegration/imaging v1.6.2
 	github.com/vektah/gqlparser/v2 v2.0.1
-	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.6.0
 )
---
+++ model/model_test.go
@@ -0,0 +1,63 @@
+package model_test
+
+import (
+	"github.com/PatrickKvartsaniy/image-processing-service/model"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestImage_IncreaseVersion(t *testing.T) {
+	var img model.Image
+	img.IncreaseVersion()
+	assert.Equal(t, 1, img.Version)
+	img.IncreaseVersion()
+	assert.Equal(t, 2, img.Version)
+}
+
+func TestImage_NewSize(t *testing.T) {
+	tc := struct {
+		newPath  string
+		newSize  model.SizeInput
+		expected []model.Resized
+	}{
+		newPath: "drive.google.com/drive/u/5/my-drive",
+		newSize: model.SizeInput{
+			Width:  100,
+			Height: 120,
+		},
+		expected: []model.Resized{
+			{
+				Path:   "drive.google.com/drive/u/5/my-drive",
+				Width:  100,
+				Height: 120,
+			},
+		},
+	}
+	var img model.Image
+	img.NewSize(tc.newPath, tc.newSize)
+	assert.Equal(t, tc.expected, img.Variety)
+}
+
+func TestSizeInput_Valid(t *testing.T) {
+	tcs := []struct {
+		in       model.SizeInput
+		expected bool
+	}{
+		{
+			in: model.SizeInput{
+				Width:  10,
+				Height: 20,
+			},
+			expected: true,
+		},
+		{
+			in: model.SizeInput{
+				Width:  -1,
+				Height: 11,
+			},
+		},
+	}
+	for _, tc := range tcs {
+		assert.Equal(t, tc.expected, tc.in.Valid())
+	}
+}
